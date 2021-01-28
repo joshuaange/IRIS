2d2c47b5453186c0bc3881fba0b9413caa5dc863
Joshua Ange <joshange@gmail.com>
2021-01-28 15:08:18 -0600

    zero consideration for vector functions

--- math/vec/fbeta.m
+++ math/vec/fbeta.m
@@ -1,4 +1,7 @@
 function [beta] = fbeta(vector)
 % Finds beta (y) angle of vector
 beta = acos((vector(2,2))/(mag(vector)));
+if vector(2,2) = 0
+    alpha = 0
+end
 end
--- math/vec/fgamma.m
+++ math/vec/fgamma.m
@@ -1,4 +1,7 @@
 function [gamma] = fgamma(vector)
 % Findz gamma (z) angle of vector
 gamma = acos((vector(2,3))/(mag(vector)));
+if vector(2,3) = 0
+    alpha = 0
+end
 end
