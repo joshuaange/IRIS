af6f518d39ec98876f3f7afcb96c351d3109f0fd
Joshua Ange <joshange@gmail.com>
2021-01-30 16:38:59 -0600

    Revising normal force applied to frictional coefficient

--- main/segment/reactions.m
+++ main/segment/reactions.m
@@ -5,9 +5,10 @@ A_G_ij = vpasolve((mag(A_g_ij))^2 + (VAL)^2 == (T*m*g)^2, VAL);
 
 % Normal
 s{iit,jit}.B_ij = vpa(B_ij);
-A_n_ij = [S_ij(1),S_ij(2),S_ij(3); 0, 0, 0];
 if jit <= Kt
     A_n_ij = [S_ij(1),S_ij(2),S_ij(3); (-(mag(s{iit,1}.B_ij))/T)/Kt * cos(falpha(B_ij)),(-(mag(s{iit,1}.B_ij))/T)/Kt * cos(fbeta(B_ij)),(-(mag(s{iit,1}.B_ij))/T)/Kt * cos(fgamma(B_ij))];
+else
+    A_n_ij = [S_ij(1),S_ij(2),S_ij(3); (-(mag(B_ij))/T)/Kt * cos(falpha(B_ij)),(-(mag(B_ij))/T)/Kt * cos(fbeta(B_ij)),(-(mag(B_ij))/T)/Kt * cos(fgamma(B_ij))];
 end
 s{iit,jit}.A_n_ij = vpa(A_n_ij);
 s{iit,jit}.S_ij = vpa(S_ij);   
@@ -15,13 +16,14 @@ s{iit,jit}.S_ij = vpa(S_ij);
 % Elasticity
 A_e_ij = [S_ij(1),S_ij(2),S_ij(3); 0, 0, 0];
 if jit <= Kt
-    A_e_ij(2,1) = ((K(s{iit,1}.S_ij(1),s{iit,1}.S_ij(2))+K(R_ij(1),R_ij(2)))/2)*((((Length(O_ij))/T)/(cos(pi/2)))*sin(pi/2)) * cos(falpha(A_n_ij));
-    A_e_ij(2,2) = ((K(s{iit,1}.S_ij(1),s{iit,1}.S_ij(2))+K(R_ij(1),R_ij(2)))/2)*((((Length(O_ij))/T)/(cos(pi/2)))*sin(pi/2)) * cos(fbeta(A_n_ij));
-    A_e_ij(2,3) = ((K(s{iit,1}.S_ij(1),s{iit,1}.S_ij(2))+K(R_ij(1),R_ij(2)))/2)*((((Length(O_ij))/T)/(cos(pi/2)))*sin(pi/2)) * cos(fgamma(A_n_ij));
+    A_e_ij(2,1) = ((K(s{iit,1}.S_ij(1),s{iit,1}.S_ij(2))+K(R_ij(1),R_ij(2)))/2)*(max(A_G_ij)+mag(s{iit,1}.A_n_ij)) * cos(falpha(A_n_ij));
+    A_e_ij(2,2) = ((K(s{iit,1}.S_ij(1),s{iit,1}.S_ij(2))+K(R_ij(1),R_ij(2)))/2)*(max(A_G_ij)+mag(s{iit,1}.A_n_ij)) * cos(fbeta(A_n_ij));
+    A_e_ij(2,3) = ((K(s{iit,1}.S_ij(1),s{iit,1}.S_ij(2))+K(R_ij(1),R_ij(2)))/2)*(max(A_G_ij)+mag(s{iit,1}.A_n_ij)) * cos(fgamma(A_n_ij));
 end
     
 % Friction
-A_f_ij = [R_ij(1),R_ij(2),R_ij(3); (-0.31875*((F(S_ij(1),S_ij(2))+F(R_ij(1),R_ij(2)))/2) * (max(A_G_ij)+mag(A_n_ij))) * cos(falpha(O_ij)),(-0.31875*((F(S_ij(1),S_ij(2))+F(R_ij(1),R_ij(2)))/2) * (max(A_G_ij)+mag(A_n_ij))) * cos(fbeta(O_ij)),(-0.31875*((F(S_ij(1),S_ij(2))+F(R_ij(1),R_ij(2)))/2) * (max(A_G_ij)+mag(A_n_ij))) * cos(fgamma(O_ij))];
+%A_f_ij = [R_ij(1),R_ij(2),R_ij(3); (-0.31875*((F(S_ij(1),S_ij(2))+F(R_ij(1),R_ij(2)))/2) * (max(A_G_ij)+mag(A_n_ij))) * cos(falpha(O_ij)),(-0.31875*((F(S_ij(1),S_ij(2))+F(R_ij(1),R_ij(2)))/2) * (max(A_G_ij)+mag(A_n_ij))) * cos(fbeta(O_ij)),(-0.31875*((F(S_ij(1),S_ij(2))+F(R_ij(1),R_ij(2)))/2) * (max(A_G_ij)+mag(A_n_ij))) * cos(fgamma(O_ij))];
+A_f_ij = [R_ij(1),R_ij(2),R_ij(3); -(((F(S_ij(1),S_ij(2))+F(R_ij(1),R_ij(2)))/2) * (mag(A_n_ij))) * cos(falpha(O_ij)),-(((F(S_ij(1),S_ij(2))+F(R_ij(1),R_ij(2)))/2) * (mag(A_n_ij))) * cos(fbeta(O_ij)),-(((F(S_ij(1),S_ij(2))+F(R_ij(1),R_ij(2)))/2) * (mag(A_n_ij))) * cos(fgamma(O_ij))];
 
 % Rotation
     % Influence
