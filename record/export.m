98c99d46bae4a6766ad5bf3914de85a3384af8cb
Joshua Ange <joshange@gmail.com>
2021-02-14 15:41:23 -0600

    Fixing exporting into Unity script

--- record/export.m
+++ record/export.m
@@ -1,14 +1,14 @@
+% Constructing Arrays
 time = 0;
 timeArray = [time];
 xArray = [double(b{1}.u_i(1,1))];
 yArray = [double(b{1}.u_i(1,2))];
 zArray = [double(b{1}.u_i(1,3))];
-
 for iit = 1:i_max
     if isempty(b{iit}) == 0
         for iteration = 1:floor(double(b{iit}.t_f_i)/t_step)
-            multitude = iteration * t_step;
-            time = time + t_step;
+            multitude = iteration * T;
+            time = time + T;
             timeArray(length(timeArray)+1,1) = time;
             xArray(length(xArray)+1,1) = vpa(b{iit}.x_i(multitude));
             yArray(length(yArray)+1,1) = vpa(b{iit}.y_i(multitude));
@@ -25,6 +25,24 @@ for iit = 1:i_max
         end
     end
 end
-
-MAIN = table(timeArray,xArray,yArray,zArray);
-writetable(MAIN,strcat(folder,'\out\arrays\positions.xlsx'));
+% Writing to file
+fid = fopen('MATLAB_Movement.cs','wt');
+fprintf(fid, 'public GameObject g;\n');
+fprintf(fid, 'Vector3[] points;\n');
+fprintf(fid, 'int index;\n');
+fprintf(fid, 'int i;\n');
+fprintf(fid, 'double deltaTime = %f;\n', T);
+fprintf(fid, 'void Start () {\n');
+fprintf(fid, '\tg = GameObject.CreatePrimitive (PrimitiveType.Cube);\n');
+fprintf(fid, '\tpoints = new Vector3[%i];\n', size(timeArray,1)-1);
+for indexValue = 1:size(timeArray,1)
+    fprintf(fid, '\tpoints [%i] = new Vector3 (%f, %f, %f);\n', indexValue-1, xArray(indexValue), yArray(indexValue), zArray(indexValue));
+end
+fprintf(fid, '}\n');
+fprintf(fid, 'void Update () {\n');
+fprintf(fid, '\tg.transform.position = Vector3.MoveTowards (g.transform.position,points[index], Time.deltaTime*2);\n');
+fprintf(fid, '\tif(g.transform.position==points[index]) {\n');
+fprintf(fid, '\t\tindex++;if(index>points.Length-1){index=0;}\n');
+fprintf(fid, '\t}\n');
+fprintf(fid, '}');
+fclose(fid);
