b42f9ffb5fce9a0d2660273631f671eebe86f4ea
Joshua Ange <joshange@gmail.com>
2021-03-31 22:21:57 -0500

    Input testing data

--- logic/auto.m
+++ logic/auto.m
@@ -57,3 +57,23 @@ global L_x L_y L_z F_x F_y F_z F_r_x F_r_y F_r_z K_x K_y K_z T_c_x T_c_y T_c_z G
 folder = pwd;
 file = 'PingPongFive';
 run(strcat(folder,'\logic\start.m'));
+
+clc
+clear all
+close all
+% Load Input .mat here
+display("Reading input .mat file");
+global L_x L_y L_z F_x F_y F_z F_r_x F_r_y F_r_z K_x K_y K_z T_c_x T_c_y T_c_z G_l_x G_l_y G_l_z T_g_x T_g_y T_g_z Y_l_x Y_l_y Y_l_z Domain Bi_Int
+folder = pwd;
+file = 'FrictionlessRacquetballOne';
+run(strcat(folder,'\logic\start.m'));
+
+clc
+clear all
+close all
+% Load Input .mat here
+display("Reading input .mat file");
+global L_x L_y L_z F_x F_y F_z F_r_x F_r_y F_r_z K_x K_y K_z T_c_x T_c_y T_c_z G_l_x G_l_y G_l_z T_g_x T_g_y T_g_z Y_l_x Y_l_y Y_l_z Domain Bi_Int
+folder = pwd;
+file = 'FrictionlessRacquetballTwo';
+run(strcat(folder,'\logic\start.m'));
