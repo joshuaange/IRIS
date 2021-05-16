3895676340ae48847b0f492034e8c3fe6eef4399
Joshua Ange <joshange@gmail.com>
2021-05-16 12:40:52 -0500

    Parachutes and dynamic gravity and atmosphere for trajectory

--- logic/trajectory/trajectory.m
+++ logic/trajectory/trajectory.m
@@ -4,6 +4,12 @@ fprintf(fid, '%% This script is automatically constructed by trajectory.m \n');
 % This function creates the position functions across axes while bearing in
 % mind the dynamic nature of gravity and atmospheric density (http://farside.ph.utexas.edu/teaching/336k/Newtonhtml/node29.html)
 
+% For x and y positions, gravity and atmospheric density are considered to
+% be constant - which works for large conditions, where the interval time
+% isn't hit by the time the horizontal velocity essentially becomes zero,
+% and works for small conditions, where it is assumed to be constant
+% anyways
+
 fplot(L_min,[0,100000]);
 hold on;
 
@@ -14,8 +20,8 @@ t_A_end = 0;
 t_A_last = -0.0000000000001; t_A_vel = u_i(2,3); t_A_pos = u_i(1,3)+L_min; %Start pos, vel, and time
 
 % x_i and y_i continuous functions
-fprintf(fid,strcat('x_i(t) = (',string(u_i(2,1)),'*Q(z_i(t)))/g(z_i(t)) * (1-exp((-g(z_i(t))*t)/Q(z_i(t)))) + ',string(u_i(1,1)),';\n'));
-fprintf(fid,strcat('y_i(t) = (',string(u_i(2,2)),'*Q(z_i(t)))/g(z_i(t)) * (1-exp((-g(z_i(t))*t)/Q(z_i(t)))) + ',string(u_i(1,2)),';\n'));
+fprintf(fid,strcat('x_i(t) = (',string(u_i(2,1)),'*Q(',string(t_A_pos),'))/g(',string(t_A_pos),') * (1-exp((-g(',string(t_A_pos),')*t)/Q(',string(t_A_pos),'))) + ',string(u_i(1,1)),';\n'));
+fprintf(fid,strcat('y_i(t) = (',string(u_i(2,2)),'*Q(',string(t_A_pos),'))/g(',string(t_A_pos),') * (1-exp((-g(',string(t_A_pos),')*t)/Q(',string(t_A_pos),'))) + ',string(u_i(1,2)),';\n'));
 % Looping through and constructing piecewise equation from intervals (z_i because there is a natural stopping point at L_min)
 fprintf(fid, 'z_i(t) = piecewise(');
 while t_A_end == 0
@@ -98,7 +104,7 @@ if parachute == 1
     fprintf(fid,'if isnan(t_A_velX) == 1\n');
     fprintf(fid,'    t_A_velX = (dx_idt(t_o+0.001)+dx_idt(t_o-0.001))/2;\n');
     fprintf(fid,'end\n');
-    fprintf(fid,'x_iParachute(t) = (t_A_velX*Q_o(z_i(t)))/g(z_i(t)) * (1-exp((-g(z_i(t))*t)/Q_o(z_i(t)))) + t_A_posX;\n');
+    fprintf(fid,'x_iParachute(t) = (t_A_velX*Q_o(z_i(t_o)))/g(z_i(t_o)) * (1-exp((-g(z_i(t_o))*t)/Q_o(z_i(t_o)))) + t_A_posX;\n');
     fprintf(fid,strcat('x_i(t) = piecewise((t<=t_o), x_i(t), (t>t_o), x_iParachute(t));\n'));
     % Y
     fprintf(fid,'t_A_velY = dy_idt(t_o); t_A_posY = y_i(t_o); %%Start pos, vel, and time\n');
@@ -106,7 +112,7 @@ if parachute == 1
     fprintf(fid,'if isnan(t_A_velY) == 1\n');
     fprintf(fid,'    t_A_velY = (dy_idt(t_o+0.001)+dy_idt(t_o-0.001))/2;\n');
     fprintf(fid,'end\n');
-    fprintf(fid,'y_iParachute(t) = (t_A_velY*Q_o(z_i(t)))/g(z_i(t)) * (1-exp((-g(z_i(t))*t)/Q_o(z_i(t)))) + t_A_posY;\n');
+    fprintf(fid,'y_iParachute(t) = (t_A_velY*Q_o(z_i(t_o)))/g(z_i(t_o)) * (1-exp((-g(z_i(t_o))*t)/Q_o(z_i(t_o)))) + t_A_posY;\n');
     fprintf(fid,strcat('y_i(t) = piecewise((t<=t_o), y_i(t), (t>t_o), y_iParachute(t));\n'));                                  
                                                                                   
     % derivatives
