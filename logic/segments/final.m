97067bb94ae2c24fa046fc9306b31e04c95e71c6
Joshua Ange <joshange@gmail.com>
2021-06-05 11:27:40 -0500

    Temperature calculations

--- logic/segments/final.m
+++ logic/segments/final.m
@@ -15,26 +15,29 @@ D_ij = ((((3*pi)/2)^(2/3))*((101.97*abs(FI_ij))^(2/3))*((M_l_ij+M_p)^(2/3))*((1/
 deltaKE_ij = 0.5*m*(mag(V_ij) - mag(v_ij))^2 + 0.5*I*(mag(Q_ij) - mag(q_ij))^2;
 KE_f_ij = KE_s_ij + deltaKE_ij;
 
-% display("...Temperature");
-% % Temperature (Conductivity) - from https://www.omnicalculator.com/physics/newtons-law-of-cooling#:~:text=Newton's%20law%20of%20cooling%20formula,-The%20Newton's%20law&text=T%20%5BK%5D%20is%20the%20temperature,object%20at%20the%20time%20t%20%2C&text=T_initial%20%5BK%5D%20is%20the%20initial,the%20time%20of%20the%20cooling.
-% if D_ij==0
-%    h_f_ij = 0; % Heat flux
-% else
-%    h_f_ij = ((T_c(S_ij(1),S_ij(2))+T_c(R_ij(1),R_ij(2)))/2)*((T_s_ij-((T_g(S_ij(1),S_ij(2))+T_g(R_ij(1),R_ij(2)))/2))/D_ij);
-% end
-% if T_s_ij==((T_g(S_ij(1),S_ij(2))+T_g(R_ij(1),R_ij(2)))/2)
-%     h_t_ij = 0; % Heat transfer coefficient
-% else
-%     h_t_ij = (h_f_ij)/(T_s_ij-((T_g(S_ij(1),S_ij(2))+T_g(R_ij(1),R_ij(2)))/2));
-% end
-% SA_g_ij = pi*( (d/2)^2 * (d/2 - D_ij)^2 ); % Area of heat transfer
-% h_c_ij = h_t_ij * (SA_g_ij / h_C); % Cooling constant
-% 
-% T_g_ij = ((T_g(S_ij(1),S_ij(2))+T_g(R_ij(1),R_ij(2)))/2); % Ground temperature
-% T_f_ij = T_g_ij+(T_s_ij-T_g_ij)*exp(-T*h_c_ij); % Final temperature conduction
-% if deltaKE_ij < 0
-%     T_f_ij = T_f_ij - ((h_R*deltaKE_ij)/(h_C*m)); % Adding temperature from shifting kinetic energy to ground
-% end
+% Newton's Law of Cooling (Conductivity) - from https://www.omnicalculator.com/physics/newtons-law-of-cooling#:~:text=Newton's%20law%20of%20cooling%20formula,-The%20Newton's%20law&text=T%20%5BK%5D%20is%20the%20temperature,object%20at%20the%20time%20t%20%2C&text=T_initial%20%5BK%5D%20is%20the%20initial,the%20time%20of%20the%20cooling.
+if D_ij==0 % Heat flux (Fourier's Law)
+   h_f_ij = 0;
+else
+   h_f_ij = (T_c(S_ij(1),S_ij(2)))*(T_s_ij-T_g(S_ij(1),S_ij(2)))/D_ij;
+end
+if T_s_ij==(T_g(S_ij(1),S_ij(2))) % Heat transfer coefficient
+    h_t_ij = 0;
+else
+    h_t_ij = (h_f_ij)/(T_s_ij-(T_g(S_ij(1),S_ij(2))));
+end
+SA_g_ij = 2*pi*(d/2)*D_ij; % Area of heat transfer (area of spherical cap)
+h_c_ij = h_t_ij * (SA_g_ij / h_C); % Cooling coefficient
+T_f_ij = (T_g(S_ij(1),S_ij(2))) + (T_s_ij - (T_g(S_ij(1),S_ij(2)))) * exp(- h_c_ij * T); % Final temperature conduction
+
+
+% Temperature gained from kinetic energy (heat capacity) https://www.omnicalculator.com/physics/specific-heat
+if deltaKE_ij < 0
+    T_f_ij = T_f_ij + (m*((1/3 * pi * (D_ij)^2 * (3*(d/2) - D_ij))/(4/3 * pi * (d/2)^3)))*((h_R*deltaKE_ij)/(h_C*m)); % Adding temperature from shifting kinetic energy to ground
+    % finds change in K by multiplying (ratio of volume * mass ) to
+    % specific heat capacity in K/kg
+end
+
 
 % % Rotation
 % I_ij = [R_ij(1),R_ij(2),R_ij(3); (q_ij(2,1)+Q_ij(2,1))/2 * T, (q_ij(2,2)+Q_ij(2,2))/2 * T, (q_ij(2,3)+Q_ij(2,3))/2 * T];
